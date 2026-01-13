b62e9c9d96c81ea71bc600686f150aa6bee0f3b9
Gwenaele Manse <gwenaelemanse@gmail.com>
2026-01-13 12:07:32 +0100

    base de données

---
+++ bdd.php
@@ -0,0 +1,14 @@
+<?php
+ 
+ function connexion() {
+    try
+    {
+        // Pensez à modifier cette ligne avec le nom de votre base de données et vos identifiants
+        return new PDO('mysql:host=localhost;dbname=artbox;charset=utf8', 'root', '');
+    }
+    catch (Exception $e)
+    {
+        die('Erreur : ' . $e->getMessage());
+    }
+}
+
