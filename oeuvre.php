07b7fb2301f2bcbd57329969fbca02738af9d707
Gwenaele Manse <gwenaelemanse@gmail.com>
2026-01-20 13:48:09 +0100

    modification sécurité

--- oeuvre.php
+++ oeuvre.php
@@ -1,31 +1,31 @@
 <?php
-    require 'header.php';
-    require 'bdd.php';
-    $db = connexion();
+require 'header.php';
+require 'bdd.php';
+$db = connexion();
 
-    // Si l'URL ne contient pas d'id, on redirige sur la page d'accueil
-    if(empty($_GET['id'])) {
-        header('Location: index.php');
-    };
+// Si l'URL ne contient pas d'id, on redirige sur la page d'accueil
+if (empty($_GET['id'])) {
+    header('Location: index.php');
+};
 
-    $requete = $db->prepare('SELECT * FROM oeuvres WHERE id=? ' );
-    $requete->execute([$_GET['id']]);
-    $oeuvre = $requete->fetch();
-    // Si aucune oeuvre trouvé, on redirige vers la page d'accueil
-    if(is_null($oeuvre)) {
-        header('Location: index.php');
-    }
+$requete = $db->prepare('SELECT * FROM oeuvres WHERE id=? ');
+$requete->execute([$_GET['id']]);
+$oeuvre = $requete->fetch();
+// Si aucune oeuvre trouvé, on redirige vers la page d'accueil
+if (is_null($oeuvre)) {
+    header('Location: index.php');
+}
 ?>
 
 <article id="detail-oeuvre">
     <div id="img-oeuvre">
-        <img src="<?= $oeuvre['image'] ?>" alt="<?= $oeuvre['titre'] ?>">
+        <img src="<?= htmlspecialchars($oeuvre['image']) ?>" alt="<?= htmlspecialchars($oeuvre['titre']) ?>">
     </div>
     <div id="contenu-oeuvre">
-        <h1><?= $oeuvre['titre'] ?></h1>
-        <p class="description"><?= $oeuvre['artiste'] ?></p>
+        <h1><?= htmlspecialchars($oeuvre['titre']) ?></h1>
+        <p class="description"><?= htmlspecialchars($oeuvre['artiste']) ?></p>
         <p class="description-complete">
-             <?= $oeuvre['description'] ?>
+            <?= htmlspecialchars($oeuvre['description']) ?>
         </p>
     </div>
 </article>
--- traitement.php
+++ traitement.php
@@ -2,37 +2,33 @@
 
 
 
-  $postdata= $_POST;
+$postdata = $_POST;
 
 
 
-  if (
-     empty($postdata['titre'])
+if (
+    empty($postdata['titre'])
     || empty($postdata['image'])
     || !filter_var($postdata['image'], FILTER_VALIDATE_URL)
     || empty($postdata['artiste'])
     || empty($postdata['description'])
     || strlen($postdata['description']) < 3
-) { header ('Location: ajouter.php?erreur=true');
-    }else{
- 
-   
-        $titre = htmlspecialchars($postdata['titre']);
-        $description = htmlspecialchars($postdata['description']);
-        $artiste = htmlspecialchars($postdata['artiste']);
-        $image = htmlspecialchars($postdata['image']);
-
-           
-        require 'bdd.php';
-         $db = connexion();
-
-  $requete = $db->prepare('INSERT INTO oeuvres (titre, description, artiste, image) VALUES (?,?,?,?)');
-  $requete->execute([$titre, $description, $artiste, $image]) ;
-
-   header('Location: oeuvre.php?id=' . $db->lastInsertId());
-
-   }
+) {
+    header('Location: ajouter.php?erreur=true');
+} else {
 
 
-?>
+    $titre = ($postdata['titre']);
+    $description = ($postdata['description']);
+    $artiste = ($postdata['artiste']);
+    $image = ($postdata['image']);
 
+
+    require 'bdd.php';
+    $db = connexion();
+
+    $requete = $db->prepare('INSERT INTO oeuvres (titre, description, artiste, image) VALUES (?,?,?,?)');
+    $requete->execute([$titre, $description, $artiste, $image]);
+
+    header('Location: oeuvre.php?id=' . $db->lastInsertId());
+}
